dd2794b297676e27873652c484235845ae81e2c0
ParveenDavid2394 <parveendavid2394@gmail.com>
2020-09-17 09:50:36 +0800

    setup feature to display food once page is loaded

--- app.js
+++ app.js
@@ -72,3 +72,31 @@ const menu = [
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
 ];
+
+
+const sectionCenter = document.querySelector('.section-center');
+
+window.addEventListener('DOMContentLoaded', displayMenuItems(menu));
+
+
+function displayMenuItems(menuItems) {
+  let display = menuItems.map( (item) => {
+
+	return `
+			<article class = "menu-item">
+				<img src = ${item.img} class = "photo" alt = ${item.title}>
+				<div class = "item-info">
+				<header>
+				<h4> ${item.title} </h4> 
+				<h4 class = "price"> ${item.price} </h4> 
+				</header> 
+				<p class = "item-text" >
+					${item.desc}
+				</p> 
+				</div> 
+			</article>`
+  });
+
+  display = display.join('');
+  sectionCenter.innerHTML = display;
+}
